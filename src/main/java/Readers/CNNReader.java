be3f6e52ec0ad0416b8c14bea8368b8dc17164a8
victorflorintsev <victorflorintsev1996@gmail.com>
2018-07-10 20:31:29 -0500

    pushing CNNReader for Evelyn to work on

---
+++ src/main/java/Readers/CNNReader.java
@@ -0,0 +1,11 @@
+package Readers;
+
+import java.net.URL;
+
+public class CNNReader extends SeleniumReader {
+    CNNReader(URL url) throws Exception {
+        super(url);
+
+        // insert code here bub!
+    }
+}
